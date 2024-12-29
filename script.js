e376c81fc79f28e6d0ee9c0bbf09edb053e5350f
yohhannees <yohannesgetachewerieso@gmail.com>
2024-12-29 11:22:16 +0300

    feat:  background color and card color picker added . hint added

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const cards = document.querySelectorAll(".card"),
-    timeTag = document.querySelector(".time b"),
-    flipsTag = document.querySelector(".flips b"),
-    refreshBtn = document.querySelector(".details button");
+  timeTag = document.querySelector(".time b"),
+  flipsTag = document.querySelector(".flips b"),
+  refreshBtn = document.querySelector(".details button"),
+  bgColorInput = document.querySelector("#bg-color-input"),
+  cardColorInput = document.querySelector("#card-color-input"),
+  applyColorsBtn = document.querySelector("#apply-colors");
 
 let maxTime = 20;
 let timeLeft = maxTime;
@@ -9,120 +12,143 @@ let flips = 0;
 let matchedCard = 0;
 let disableDeck = false;
 let isPlaying = false;
-let cardOne, cardTwo, timer;
+let cardOne, cardTwo, timer, hintTimer;
 
 function showModal(msgbox) {
-    let modal = document.getElementById("modal");
-    let message = document.getElementById(msgbox);
-    modal.style.display = "block";
-    message.style.display = "block";
+  let modal = document.getElementById("modal");
+  let message = document.getElementById(msgbox);
+  modal.style.display = "block";
+  message.style.display = "block";
+}
+
+function closeModal() {
+  let modal = document.getElementById("modal");
+  let messageBoxes = document.getElementsByClassName("message-box");
+  modal.style.display = "none";
+  for (let i = 0; i < messageBoxes.length; i++) {
+    messageBoxes[i].style.display = "none";
   }
- 
-  function closeModal() {
-    let modal = document.getElementById("modal");
-    let messageBoxes = document.getElementsByClassName("message-box");
-    modal.style.display = "none";
-    for (let i = 0; i < messageBoxes.length; i++) {
-      messageBoxes[i].style.display = "none";
-    }
-  }
-  let closeBtn = document.getElementById("close-btn");
-  closeBtn.addEventListener("click", closeModal);
-  
+}
+let closeBtn = document.getElementById("close-btn");
+closeBtn.addEventListener("click", closeModal);
+
 function initTimer() {
-    if (timeLeft <= 0) {
-        if (matchedCard == 6) {
-            showModal("win-box")
-        } else {
-            showModal("lose-box")
-        }
-        return clearInterval(timer);
+  if (timeLeft <= 0) {
+    if (matchedCard == 6) {
+      showModal("win-box");
+    } else {
+      showModal("lose-box");
     }
-    timeLeft -= 1;
-    timeTag.innerText = timeLeft;
+    return clearInterval(timer);
+  }
+  timeLeft -= 1;
+  timeTag.innerText = timeLeft;
 }
 
 function flipCard({ target: clickedCard }) {
-    if (!isPlaying) {
-        isPlaying = true;
-        timer = setInterval(initTimer, 1000);
+  if (!isPlaying) {
+    isPlaying = true;
+    timer = setInterval(initTimer, 1000);
+    hintTimer = setInterval(showHint, 4000);
+  }
+  if (clickedCard !== cardOne && !disableDeck && timeLeft > 0) {
+    flips++;
+    flipsTag.innerText = flips;
+    clickedCard.classList.add("flip");
+    if (!cardOne) {
+      return (cardOne = clickedCard);
     }
-    if (clickedCard !== cardOne && !disableDeck && timeLeft > 0) {
-        flips++;
-        flipsTag.innerText = flips;
-        clickedCard.classList.add("flip");
-        if (!cardOne) {
-            return cardOne = clickedCard;
-        }
-        cardTwo = clickedCard;
-        disableDeck = true;
-        let cardOneImg = cardOne.querySelector(".back-view img").src,
-            cardTwoImg = cardTwo.querySelector(".back-view img").src;
-        matchCards(cardOneImg, cardTwoImg);
-    }
+    cardTwo = clickedCard;
+    disableDeck = true;
+    let cardOneImg = cardOne.querySelector(".back-view img").src,
+      cardTwoImg = cardTwo.querySelector(".back-view img").src;
+    matchCards(cardOneImg, cardTwoImg);
+  }
 }
 
 function matchCards(img1, img2) {
-    if (img1 === img2) {
-        matchedCard++;
-        if (matchedCard == 6 && timeLeft > 0) {
-            showModal("win-box")
-            return clearInterval(timer);
-        }
-        cardOne.removeEventListener("click", flipCard);
-        cardTwo.removeEventListener("click", flipCard);
-        cardOne = cardTwo = "";
-        return disableDeck = false;
+  if (img1 === img2) {
+    matchedCard++;
+    if (matchedCard == 6 && timeLeft > 0) {
+      showModal("win-box");
+      clearInterval(timer);
+      clearInterval(hintTimer);
     }
+    cardOne.removeEventListener("click", flipCard);
+    cardTwo.removeEventListener("click", flipCard);
+    cardOne = cardTwo = "";
+    return (disableDeck = false);
+  }
 
-    setTimeout(() => {
-        cardOne.classList.add("shake");
-        cardTwo.classList.add("shake");
-    }, 400);
+  setTimeout(() => {
+    cardOne.classList.add("shake");
+    cardTwo.classList.add("shake");
+  }, 400);
 
-    setTimeout(() => {
-        cardOne.classList.remove("shake", "flip");
-        cardTwo.classList.remove("shake", "flip");
-        cardOne = cardTwo = "";
-        disableDeck = false;
-    }, 1200);
+  setTimeout(() => {
+    cardOne.classList.remove("shake", "flip");
+    cardTwo.classList.remove("shake", "flip");
+    cardOne = cardTwo = "";
+    disableDeck = false;
+  }, 1200);
 }
 
 function shuffleCard() {
-    timeLeft = maxTime;
-    flips = matchedCard = 0;
-    cardOne = cardTwo = "";
-    clearInterval(timer);
-    timeTag.innerText = timeLeft;
-    flipsTag.innerText = flips;
-    disableDeck = isPlaying = false;
+  timeLeft = maxTime;
+  flips = matchedCard = 0;
+  cardOne = cardTwo = "";
+  clearInterval(timer);
+  clearInterval(hintTimer);
+  timeTag.innerText = timeLeft;
+  flipsTag.innerText = flips;
+  disableDeck = isPlaying = false;
 
-    let arr = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6];
-    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+  let arr = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6];
+  arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
 
-    cards.forEach((card, index) => {
-        card.classList.remove("flip");
-        let imgTag = card.querySelector(".back-view img");
-        setTimeout(() => {
-            imgTag.src = `images/img-${arr[index]}.png`;
-        }, 500);
-        card.addEventListener("click", flipCard);
-    });
+  cards.forEach((card, index) => {
+    card.classList.remove("flip");
+    let imgTag = card.querySelector(".back-view img");
+    setTimeout(() => {
+      imgTag.src = `images/img-${arr[index]}.png`;
+    }, 500);
+    card.addEventListener("click", flipCard);
+  });
 }
+
+function showHint() {
+  let unmatchedCards = Array.from(cards).filter(
+    (card) => !card.classList.contains("flip")
+  );
+  if (unmatchedCards.length > 0) {
+    let randomCard =
+      unmatchedCards[Math.floor(Math.random() * unmatchedCards.length)];
+    randomCard.classList.add("flip");
+    setTimeout(() => {
+      randomCard.classList.remove("flip");
+    }, 500);
+  }
+}
+
 let timeInput = document.querySelector("#time-input"),
-    timeSubmit = document.querySelector("#time-submit");
+  timeSubmit = document.querySelector("#time-submit");
 
 timeSubmit.addEventListener("click", function () {
-    maxTime = parseInt(timeInput.value);
-    timeLeft = maxTime;
-    timeInput.value = "";
+  maxTime = parseInt(timeInput.value);
+  timeLeft = maxTime;
+  timeInput.value = "";
 });
 shuffleCard();
 
 refreshBtn.addEventListener("click", shuffleCard);
 
-cards.forEach(card => {
-    card.addEventListener("click", flipCard);
+cards.forEach((card) => {
+  card.addEventListener("click", flipCard);
 });
 
-
+applyColorsBtn.addEventListener("click", function () {
+  document.body.style.backgroundColor = bgColorInput.value;
+  cards.forEach((card) => {
+    card.querySelector(".view").style.backgroundColor = cardColorInput.value;
+  });
+});
